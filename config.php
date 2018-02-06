3ee72196f583b3ebd808ff586d7504847ca333fe
Tomasz Antczak <tomek.antczak@gmail.com>
2018-02-06 15:28:52 +0100

    commit 1

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
-<?
+<?php
+
 setlocale(LC_ALL, 'pl_PL.utf8');
 
 mb_internal_encoding('UTF-8');
 
 mb_regex_encoding('UTF-8');
-?>
--- run.php
+++ run.php
@@ -1,26 +1,26 @@
 #!/usr/bin/php
 <?php
 
-require_once 'lib/simple_html_dom.inc.php';
-require_once 'config.inc.php';
+require_once 'lib/simple_html_dom.php';
+require_once 'config.php';
 
-$__p_html=file_get_contents('source_html.txt') or die();
+$__p_html = file_get_contents('source_html.txt') or die();
 
-$__html=str_get_html($__p_html);
+$__html = str_get_html($__p_html);
 
 if (!$__html) die();
 
-$__art=array_shift($__html->find('div#art'));
+$__art = array_shift($__html->find('div#art'));
 
 if ($__art) {
-	foreach($__art->find('table,script,li') as $__p) {
-		$__p->remove();
-	}
-	print $__art->innertext;
+
+    foreach ($__art->find('table,script,li') as $tag) {
+        $tag->remove();
+    }
+
+    print $__art->innertext . "\n";
 }
 
 $__html->clear();
 
 unset($__html);
-
-?>
